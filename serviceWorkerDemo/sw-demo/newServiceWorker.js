43f915f0f7a00b2a02e674d1c77ddb47ad4bb9f8
Kelvin Okuroemi <76855464+KelvinOkuroemi@users.noreply.github.com>
2023-03-02 17:19:41 +0100

    Update newServiceWorker.js

--- serviceWorkerDemo/sw-demo/newServiceWorker.js
+++ serviceWorkerDemo/sw-demo/newServiceWorker.js
@@ -1,1 +1,43 @@
 
+const cachedName = "siteVersion";
+
+// Step - Install Service Worker
+self.addEventListener("install", (e) => {
+  console.log("Service Worker Installed");
+  // Step - Cache files
+});
+// Step - Activate Service Worker
+self.addEventListener("activate", (e) => {
+  console.log("Service Worker Activated");
+  //Step - Keep Cache light
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== cachedName) {
+            console.log("Cached Service worker is being cleared");
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Step - Persisting cached assets
+self.addEventListener("fetch", (e) => {
+  console.log("fetching service worker");
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const responseClone = res.clone();
+        caches.open(cachedName).then((cache) => {
+          cache.put(e.request, responseClone);
+        });
+        return res;
+      })
+      .catch((err) => {
+        caches.match(e.request).then((res) => res);
+      })
+  );
+});
